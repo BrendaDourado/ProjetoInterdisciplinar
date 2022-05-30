1a16500477fb65ab76788407ab6f885ce92a4751
Jared Ricioli <jared.ricioli@live.com>
2022-05-30 19:43:08 -0300

    feat/table com instituicao

--- src/views/InstituicoesParceiras.vue
+++ src/views/InstituicoesParceiras.vue
@@ -1,6 +1,13 @@
 <template>
 <div>
-    <h1>Colocar aqui a lista e instituições parceiras</h1>
+    <h1>Instituições parceiras</h1>
+
+      <v-data-table
+    :headers="headers"
+    :items="desserts"
+    :items-per-page="5"
+    class="elevation-1 mb-4 mr-4 ml-4 mt-4"
+  ></v-data-table>
 
  <v-btn class= "mb-4 btn" @click="$router.push('/CadastroInstituicao')">
 
@@ -13,6 +20,25 @@
 export default {
   data() {
     return {
+      headers: [
+          {
+            text: 'Instituição',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Endereço', value: 'endereço' },
+          { text: 'Funcionamento', value: 'func' },
+          { text: 'Responsavel', value: 'resp' },
+        ],
+        desserts: [
+          {
+            name: 'CRECHE COMUNITÁRIA RAIO DE SOL',
+            endereço: 'Avenida Nova Cantareira, 1201',
+            func: "9h as 18h",
+            resp: "Adônis",
+          },
+          ]
     };
   },
   methods: {
