efe66c5874e6c1efef7c31d0db6f428a2026c3a2
Brenda Paulla Dourado de Oliveira <brenda.dourado@iteris.com.br>
2022-05-30 19:45:24 -0300

    atualização na confirma cadastro

--- src/views/ConfirmaCadastro.vue
+++ src/views/ConfirmaCadastro.vue
@@ -1,16 +1,22 @@
 <template>
-    <div>
-    <h1>OLÁ {{name}} CADASTRO CONFIRMADO !!! SEJA BEM VINDO AO DONAVERSE</h1>
-    <h3> para seguir para área logada </h3>
-    <v-btn color="blue" @click="$router.push('/HomeLogada')">  CLIQUE AQUI </v-btn>
-    </div>
+  <div>
+    <br/>
+    <h1>OLÁ {{ name }} CADASTRO CONFIRMADO !!! SEJA BEM VINDO AO DONAVERSE</h1>
+    <br/>
+    <br/>
+    <h3>para seguir para área logada</h3>
+      <br>
+    <v-btn color="blue" @click="$router.push('/HomeLogada')">
+      CLIQUE AQUI
+    </v-btn>
+  </div>
 </template>
 <script>
 export default {
-    data() {
-        return{
-            name: localStorage.getItem("nome")
-        }  
-    },
-}
+  data() {
+    return {
+      name: localStorage.getItem("nome"),
+    };
+  },
+};
 </script>
